c8f577a7173853a2593270f88bbbee42bfc4262e
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-05-19 20:12:43 +0530

    adding heading style

--- src/components/header/CustomButton.jsx
+++ src/components/header/CustomButton.jsx
@@ -2,22 +2,43 @@ import { Box,Button, Typography, styled} from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Wrapper=styled(Box)`
-display:'flex';
+display:flex;
 margin:0 3% 0 auto;
+& > button, & > p, & > div{
+    margin-right:40px;
+    font-size:16px;
+    align-item:center;
+}
+`
+
+const Container=styled(Box)`
+display:flex;
+`
+
+const LoginButton=styled(Button)`
+color:#2874f0;
+background:#fff;
+text-transform:none;
+padding:5px 40px;
+box-shadow:none;
+font-weight:600;
+height:32px;
+
+
 `
 
 export const CustomButtons=()=>{
     return(
         <Wrapper>
-           <Button variant='contained'>Login</Button>
+           <LoginButton variant='contained'>Login</LoginButton>
 
-           <Typography>Become a Seller</Typography>
-           <Typography>More</Typography>
+           <Typography style={{margin:3, width:135}}>Become a Seller</Typography>
+           <Typography style={{margin:3,marginRight:40}}>More</Typography>
            
-           <Box>
+           <Container>
             <ShoppingCartIcon/>
             <Typography>Cart</Typography>
-           </Box>
+           </Container>
         </Wrapper>
     )
 }
